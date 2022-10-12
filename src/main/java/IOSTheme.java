c59339c012c29fa7084279508ee1e7fd05f3cfca
kevinabdila <113450062+kevinabdila@users.noreply.github.com>
2022-10-12 14:22:59 +0700

    Create IOSTheme.java

---
+++ src/main/java/IOSTheme.java
@@ -0,0 +1,15 @@
+public class IOSTheme implements Theme {
+
+    @Override
+    public void print() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onClick() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
