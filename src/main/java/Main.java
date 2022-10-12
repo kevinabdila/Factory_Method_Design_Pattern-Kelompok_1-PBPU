8eea61d244ef6fce1f804093dfedc7c2b765e448
Ilham Nuansa <77243888+ilhamn65@users.noreply.github.com>
2022-10-12 12:44:48 +0700

    first push

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author hp
+ */
+public class Main {
+    public static void main(String []args){
+        WinButton winbutton1 = new WinButton();
+        
+        winbutton1.print();
+    }
+}
