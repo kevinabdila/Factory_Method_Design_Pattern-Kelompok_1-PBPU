d49cbb71e83ca1ec928565011c848e4edd8b5215
kevinabdila <113450062+kevinabdila@users.noreply.github.com>
2022-10-12 14:10:34 +0700

    Create AndroidWallpaper.java

---
+++ src/main/java/AndroidWallpaper.java
@@ -0,0 +1,10 @@
+public class AndroidWallpaper implements Wallpaper {
+    public void print() {
+        System.out.println("Ini Wallpaper Android");
+        onClick();
+    }
+    
+    public void onClick() {
+        System.out.println("Wallpaper android");
+    }
+}
