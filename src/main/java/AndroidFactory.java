be8ead55bf5cc4098423de03809e7945dec770e8
kevinabdila <113450062+kevinabdila@users.noreply.github.com>
2022-10-12 14:10:27 +0700

    Create AndroidFactory.java

---
+++ src/main/java/AndroidFactory.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class AndroidFactory extends OSFactory {
+
+    @Override
+    public Theme createTheme() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Wallpaper createWallpaper() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
